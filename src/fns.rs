0c7e7c8b0d80dcb5c185d95a1c36710043026732
skelegorg <62708878+skelegorg@users.noreply.github.com>
2022-06-17 17:17:10 -0400

    a;sdfiea

--- src/fns.rs
+++ src/fns.rs
@@ -2,11 +2,18 @@
 
 pub struct Task {
     pub name: String,
-    pub due: Option<i64>,
-    pub created_on: i64,
+    pub due: Option<chrono::DateTime<chrono::Utc>>,
+    pub created_on: chrono::DateTime<chrono::Utc>,
 }
 
-pub fn add_to_file(_task: Task) {
-    println!("hello");
+// convert the input time string to a Utc time obj for the due time
+pub fn time_string_to_date_time(_current_time: chrono::DateTime<chrono::Utc>, _due_time: String) -> Option<chrono::DateTime<chrono::Utc>> {
+    if _due_time == String::from("NONE") {
+        return None
+    }
+    Some(chrono::Utc::now())
 }
 
+pub fn add_task(task: Task) {
+    println!("adding task {}", task.name);
+}
--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,30 @@ fn main() {
                 println!("Don't forget a task name. Try todo --help!");
                 return
             }
+            let mut final_n = 0;
             let mut task_name: String = args[2].to_owned();
             for n in 3..args.len() {
                 if args[n].starts_with("-") {
+                    final_n = n;
                     break
                 }
                 task_name = format!("{} {}", task_name, args[n].to_owned());
             }
-            println!("{}", task_name);
+            let mut due_string: String = String::new();
+            if args.contains(&String::from("--due")) {
+                for n in final_n..args.len() {
+                    due_string = format!("{} {}", due_string, args[n].to_owned());
+                }
+            } else {
+                due_string = String::from("NONE")
+            }
+            let f_due_string = fns::time_string_to_date_time(_current_time, due_string);
+            let new_task: fns::Task = fns::Task {name: task_name, due: f_due_string, created_on: _current_time};
+            fns::add_task(new_task);
             return
         }
         _ => {
             println!("jekasd");
         }
     }
-    println!("{:?}",args);
 }
